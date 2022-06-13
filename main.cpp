30bdc87923070d97998d7fed725dedaa6eb2a8c7
Daniils Ternoviks <98739266+ImpulseLv@users.noreply.github.com>
2022-06-13 19:42:20 +0000

    Pielagots pirmais jautajums!

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ int main() {
 
 cout<<"Tā ir viktorina par tēmu Biežāk lietotās funkcijas darbam ar simboliem un simbolu virknēm programmēšanas valodā C++";
   
-cout<<"\n\n\n1."
+cout<<"\n\n\n1.Kuru simbola virknēs funkciju izmanto lai noteikt norādītās virknes garumu, izņemot nulles rakstzīmi?";
   
 }
