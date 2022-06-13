5f305da3a5f92b3e3c4eb5102cbb0923951578bc
Daniils Ternoviks <98739266+ImpulseLv@users.noreply.github.com>
2022-06-13 19:40:42 +0000

    Iesakts darbs!

--- main.cpp
+++ main.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main() {
 
-
-
+cout<<"Tā ir viktorina par tēmu Biežāk lietotās funkcijas darbam ar simboliem un simbolu virknēm programmēšanas valodā C++";
+  
+cout<<"\n\n\n1."
   
 }
